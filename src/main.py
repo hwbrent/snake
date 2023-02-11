88d1a478257b659b3347dd460a02c99f7ebbe981
mustcat <hwbrent@icloud.com>
2023-02-11 09:14:40 +0000

    Write printing function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,26 @@
+import os
+import random
+
+
+def print_to_stdout(stdout, snake, food):
+    for x, y in snake:
+        stdout[y][x] = "S"
+
+    foodx, foody = food
+    stdout[foody][foodx] = "F"
+
+    for line in stdout:
+        print("".join(line))
+
+
 def main():
-    pass
+    columns, lines = os.get_terminal_size()
+    snake = [[columns // 2, lines // 2]]
+    food = [random.randint(0, columns), random.randint(0, lines)]
+    stdout = [[" " for y in range(columns)] for x in range(lines)]
+
+    print_to_stdout(stdout, snake, food)
+
 
 if __name__ == "__main__":
     main()
