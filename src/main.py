2580bd5c90a922653ec067db2144acabe29fa749
mustcat <hwbrent@icloud.com>
2023-02-12 19:03:05 +0000

    Implement random placing of food

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import os
 import random
 import keyboard
 import time
+import copy
+import sys
 
 
 def print_to_stdout(stdout, snake, food):
@@ -37,6 +39,37 @@ def process_key_press(direction):
         direction[0], direction[1] = 1, 0
 
 
+def move_food(food, snake, columns, lines):
+    snake_xs = (seg[0] for seg in snake)
+    snake_ys = (seg[1] for seg in snake)
+
+    poss_xs = list(x for x in range(columns) if not x in snake_xs and x != food[0])
+    poss_ys = list(y for y in range(lines) if not y in snake_ys and y != food[1])
+
+    random_x = random.randint(min(poss_xs), max(poss_xs) - 1)
+    random_y = random.randint(min(poss_ys), max(poss_ys) - 1)
+
+    food[0], food[1] = random_x, random_y
+
+
+def test():
+    columns, lines = os.get_terminal_size()
+    snake = [[columns // 2, lines // 2]]
+    direction = [1, 0]
+
+    food = [random.randint(0, columns), random.randint(0, lines)]
+    stdout = [[" " for y in range(columns)] for x in range(lines)]
+
+    while True:
+        print_to_stdout(stdout, snake, food)
+
+        # Check to see if there was a key press, and if necessary, adjust
+        # the direction of movement of the snake.
+        process_key_press(direction)
+        move_food(food, snake, columns, lines)
+        time.sleep(0.0125)
+
+
 def main():
     columns, lines = os.get_terminal_size()
     snake = [[columns // 2, lines // 2]]
@@ -52,9 +85,31 @@ def main():
         # the direction of movement of the snake.
         process_key_press(direction)
 
-        # Move the head of the snake.
-        snake[0][0] += direction[0]
-        snake[0][1] += direction[1]
+        # Move the snake.
+        preceding_seg = None
+        for i, part in enumerate(snake):
+            if i == 0:
+                head = part
+                preceding_seg = copy.deepcopy(head)
+                head[0] += direction[0]
+                head[1] += direction[1]
+            else:
+                current_seg = snake[i]
+                current_copy = copy.deepcopy()
+
+                current_seg[0], current_seg[0] = preceding_seg[0], preceding_seg[1]
+                preceding_seg[0], preceding_seg[0] = current_copy[0], current_copy[1]
+
+        # If the snake head is over the food, increment its length.
+        if snake[0] == food:
+            snake.append(preceding_seg)
+            move_food(food, snake, columns, lines)
+
+        # If the snake ate itself.
+        if snake[0] in snake[1:]:
+            print("GAME OVER")
+            sys.exit()
+        #
 
         # Move the rest of the parts of the snake to be where the preceding
         # part was in the step before.
@@ -64,4 +119,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    test()
