db56625f7cf56a13eab08bbba33bc428859f1bbd
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-03-04 18:52:57 +0000

    Add functions to set and print snake

--- src/main.c
+++ src/main.c
@@ -18,10 +18,34 @@ void clear_stdout() {
     system("clear");
 }
 
+// The coordinates of each segment of the snake.
+int snake_rows[81];
+int snake_cols[81];
+
+void set_initial_snake_position() {
+    int row_num = rows/2;
+    int col_num = cols/2;
+
+    snake_rows[0] = row_num;
+    snake_cols[0] = col_num;
+
+    for (int i = 1; i < 81; i++) {
+        snake_rows[i] = -1;
+        snake_cols[i] = -1;
+    }
+}
+
+void print_snake() {
+    for (int i = 0; i < 81; i++) {
+        printf("[%d, %d], ", snake_rows[i], snake_cols[i]);
+    }
+    printf("\n");
+}
+
 int main (int argc, char **argv)
 {
     get_terminal_dimensions();
-    printf ("rows %d\n", rows);
-    printf ("cols %d\n", cols);
-    return 0;  // make sure your main returns int
+    set_initial_snake_position();
+    print_snake();
+    return 0;
 }
