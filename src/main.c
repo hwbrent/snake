9c66ca8392cf24f0a077e63dc40e89c9586b92fd
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-03-04 18:31:03 +0000

    Add funcs to get terminal size and clear terminal

--- src/main.c
+++ src/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
 
-int main() {
-    printf("hi");
-    // system("clear");
-    return 0;
+// The dimensions of the terminal in pixels.
+int rows;
+int cols;
+void get_terminal_dimensions() {
+    // See https://stackoverflow.com/a/1022961
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    rows = w.ws_row;
+    cols = w.ws_col;
+}
+
+void clear_stdout() {
+    system("clear");
+}
+
+int main (int argc, char **argv)
+{
+    get_terminal_dimensions();
+    printf ("rows %d\n", rows);
+    printf ("cols %d\n", cols);
+    return 0;  // make sure your main returns int
 }
