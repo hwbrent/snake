50b0f94b39d7fb4dd07725c2b92825de64c8c0ca
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-03-12 21:07:35 +0000

    Add struct to store terminal dimensions

--- src/screen/screen.c
+++ src/screen/screen.c
@@ -4,9 +4,20 @@
 
 #include "screen.h"
 
+struct terminal_window {
+    int rows;
+    int cols;
+};
+
+struct terminal_window terminal;
+
 void get_terminal_dimensions() {
     // See https://stackoverflow.com/a/1022961
     struct winsize w;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-    printf("\nscreen -> %d x %d\n\n", w.ws_row, w.ws_col);
+
+    terminal.rows = w.ws_row;
+    terminal.cols = w.ws_col;
+
+    printf("\nscreen -> %d x %d\n\n", terminal.rows, terminal.cols);
 }
