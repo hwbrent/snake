5616bbad5804d283f7e21a181c90534c1fcd75f9
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-03-19 14:13:34 +0000

    Init screen and snake

--- src/main.c
+++ src/main.c
@@ -3,49 +3,66 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 
-// The dimensions of the terminal in pixels.
-int rows;
-int cols;
-void get_terminal_dimensions() {
+struct screen {
+    int rows;
+    int cols;
+    int total_pixel_count;
+    char* pixels;
+};
+struct screen screen;
+
+void init_screen() {
     // See https://stackoverflow.com/a/1022961
     struct winsize w;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-    rows = w.ws_row;
-    cols = w.ws_col;
+
+    screen.rows = w.ws_row;
+    screen.cols = w.ws_col;
+    screen.total_pixel_count = screen.rows * screen.cols;
+
+    screen.pixels = (char*)malloc(screen.total_pixel_count * sizeof(char));
 }
 
 void clear_stdout() {
     system("clear");
 }
 
-// The coordinates of each segment of the snake.
-int snake_rows[81];
-int snake_cols[81];
+struct snake {
+    int* row_coords;
+    int* col_coords;
+};
 
-void set_initial_snake_position() {
-    int row_num = rows/2;
-    int col_num = cols/2;
+struct snake snake;
 
-    snake_rows[0] = row_num;
-    snake_cols[0] = col_num;
+void init_snake() {
+    // Assume init_screen() has been called.
 
-    for (int i = 1; i < 81; i++) {
-        snake_rows[i] = -1;
-        snake_cols[i] = -1;
-    }
+    snake.row_coords = (int*)malloc(screen.total_pixel_count * sizeof(int));
+    snake.col_coords = (int*)malloc(screen.total_pixel_count * sizeof(int));
+
+    // Set initial position in middle of screen.
+    snake.row_coords[0] = screen.rows / 2;
+    snake.col_coords[0] = screen.cols / 2;
 }
 
-void print_snake() {
-    for (int i = 0; i < 81; i++) {
-        printf("[%d, %d], ", snake_rows[i], snake_cols[i]);
-    }
-    printf("\n");
+void free_all() {
+    free(screen.pixels);
+
+    free(snake.row_coords);
+    free(snake.col_coords);
 }
 
 int main (int argc, char **argv)
 {
-    get_terminal_dimensions();
-    set_initial_snake_position();
-    print_snake();
+    printf("\n\n");
+
+    init_screen();
+    printf("screen: %d rows x %d cols\n", screen.rows, screen.cols);
+
+    init_snake();
+    printf("snake starting position: row %d, col %d\n", snake.row_coords[0], snake.col_coords[0]);
+
+    free_all();
+    printf("\n\n");
     return 0;
 }
