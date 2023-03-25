f1f241c1937eb8ff8b9f8b1a0b8ea9d2c028ee07
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-03-25 21:40:06 +0000

    Add screen & input handling funcs

--- src/main.c
+++ src/main.c
@@ -6,12 +6,71 @@
 // The dimensions of the screen.
 int rows, cols;
 
-int main(int argc, char **argv) {
+// Stores value of inputted chars.
+int c;
+
+void init_screen() {
+    // Init the ncurses screen.
     initscr();
+
+    // Get the terminal size.
     getmaxyx(stdscr,rows,cols);
 
-    refresh();
-    int d = getch();
+    cbreak();
+
+    // Pick up extra keyboard keys (arrows, F1-12, etc.)
+    keypad(stdscr, TRUE);
+
+    noecho();
+}
+
+void terminate_screen() {
     endwin();
+}
+
+int handle_key_press() {
+    c = getch();
+
+    switch (c) {
+        case 'q':
+            return 1;
+
+        case 'w':
+        case KEY_UP:
+            printw("Pressed up\n");
+            break;
+
+        case 'a':
+        case KEY_LEFT:
+            printw("Pressed left\n");
+            break;
+
+        case 's':
+        case KEY_DOWN:
+            printw("Pressed down\n");
+            break;
+
+        case 'd':
+        case KEY_RIGHT:
+            printw("Pressed right\n");
+            break;
+
+        default:
+            break;
+    }
+
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    init_screen();
+
+    int should_quit = handle_key_press();
+    if (!should_quit) {
+        refresh();
+        getch();
+    }
+
+    terminate_screen();
     return 0;
 }
