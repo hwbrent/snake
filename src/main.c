27e07b24a404bf964ee2ab6931bee35cfabc7286
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-03-25 22:03:09 +0000

    Incorporate screen logic from 'c' branch

--- src/main.c
+++ src/main.c
@@ -3,29 +3,81 @@
 #include <stdbool.h>
 #include <ncurses.h>
 
-// The dimensions of the screen.
-int rows, cols;
-
-// Stores value of inputted chars.
 int c;
 
+struct screen {
+    int rows;
+    int cols;
+    int total_pixel_count;
+    char* pixels;
+} screen;
+
+// Converts a row,col coordinate to an absolute screen coordinate.
+// e.g. if the screen is 10 rows by 108 cols, then the screen
+// coordinate [3, 22] converts to 346
+int ind(row, col) {
+    return (screen.cols * row) + col;
+}
+
+// Sets the pixel in [row, col] with 'value'.
+void set_pixel(row, col, value) {
+    screen.pixels[ind(row,col)] = value;
+}
+
 void init_screen() {
-    // Init the ncurses screen.
+    /* ncurses initialisation details. */
     initscr();
-
-    // Get the terminal size.
-    getmaxyx(stdscr,rows,cols);
-
     cbreak();
-
-    // Pick up extra keyboard keys (arrows, F1-12, etc.)
     keypad(stdscr, TRUE);
-
     noecho();
+
+    /* My custom initialisation details. */
+    getmaxyx(stdscr, screen.rows, screen.cols);
+    screen.total_pixel_count = screen.rows * screen.cols;
+    screen.pixels = (char*)malloc(screen.total_pixel_count * sizeof(char));
+
+    // Initialise the screen borders.
+    for (int i = 0; i < screen.rows; i++) {
+
+        // If it's the first or last row, every pixel will be a hashtag.
+        if (i == 0 || i == screen.rows - 1) {
+            for (int j = 0; j < screen.cols; j++) {
+                set_pixel(i, j, '#');
+            }
+
+        // Else, only the first and last pixels will be hashtags. The rest
+        // will be empty space.
+        } else {
+            set_pixel(i, 0, '#');
+            // for (int j = 1; j < screen.cols-2; j++) {
+            //     set_pixel(i, screen.cols-1, ' ');
+            // }
+            set_pixel(i, screen.cols-1, '#');
+        }
+    }
+}
+
+void print_screen() {
+    // Reset the value of any pixel that isn't a border.
+    for (int i = 1; i < screen.rows-1; i++) {
+        for (int j = 1; j < screen.cols-1; j++) {
+            set_pixel(i, j, ' ');
+        }
+    }
+
+    // Now actually print screen.
+    for (int i = 0; i < screen.rows; i++) {
+        for (int j = 0; j < screen.cols; j++) {
+            char pixel = screen.pixels[ind(i,j)];
+            mvaddch(i,j,pixel);
+        }
+    }
+    refresh();
 }
 
 void terminate_screen() {
     endwin();
+    free(screen.pixels);
 }
 
 int handle_key_press() {
@@ -65,11 +117,8 @@ int handle_key_press() {
 int main(int argc, char **argv) {
     init_screen();
 
-    int should_quit = handle_key_press();
-    if (!should_quit) {
-        refresh();
-        getch();
-    }
+    print_screen();
+    getch();
 
     terminate_screen();
     return 0;
