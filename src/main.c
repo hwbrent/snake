040836abab77e73263f71eb88ead91e7a085fcd7
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-03-27 10:32:40 +0100

    Add 'pixel' type

--- src/main.c
+++ src/main.c
@@ -3,30 +3,27 @@
 #include <stdbool.h>
 #include <ncurses.h>
 
-struct coord {
+typedef struct pixel {
     int row;
     int col;
     char value;
-};
+} pixel;
 
 struct screen {
     int rows; // the number of rows in the screen
     int cols; // the number of columns in the screen
-    char* pixels; // the char value that each pixel in the screen holds
-    int* empty_pixel_rows; // the row coordinates of the empty (' ') pixels in the screen
-    int* empty_pixel_cols; // the column coordinates of the empty (' ') pixels in the screen
+    pixel* pixels; // the char value that each pixel in the screen holds
+    int* empty_pixels; // indices of pixels in 'pixels' whose 'value' is an empty char.
 } screen;
 
 struct snake {
-    int* row_coords;
-    int* col_coords;
-    int head[2];
+    pixel* segments;
+    pixel head;
     bool should_add_seg;
 } snake;
 
 struct food {
-    int row;
-    int col;
+    pixel coord;
 } food;
 
 int main(int argc, char **argv) {
