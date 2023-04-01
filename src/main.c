ac11aeb84ae7bbaa62b1996f2e53094f756879de
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-04-01 17:31:06 +0100

    Add comments for snake

--- src/main.c
+++ src/main.c
@@ -122,18 +122,18 @@ void terminate_food() {}
 void init_snake() {
     snake.length = SNAKE_INITIAL_LENGTH;
 
+    // Initialise memory for snake segments.
     snake.rows = malloc(snake.length * sizeof *snake.rows);
-    snake.rows[0] = screen.rows / 2;
-
     snake.cols = malloc(snake.length * sizeof *snake.rows);
+
+    // Initialise head of snake to be in centre of screen.
+    snake.rows[0] = screen.rows / 2;
     snake.cols[0] = screen.cols / 2;
 
-    mvaddch(
-        snake.rows[0],
-        snake.cols[0],
-        SNAKE
-    );
+    // Print the snake head.
+    mvaddch(snake.rows[0], snake.cols[0], SNAKE);
 
+    // Initialise the snake direction of movement (left horizontal).
     snake.direction[0] = 0;
     snake.direction[1] = -1;
 }
@@ -144,43 +144,49 @@ void terminate_snake() {
 }
 
 void move_snake() {
+    // The overall idea is that the head of the snake is moved according to
+    // 'snake.direction', and each other segment takes the previous
+    // position of its preceding segment.
+
     int prev[2];
 
     bool ate_food = false;
 
-    for (int i = 0; i < snake.length; i++) {
-        if (i == 0) {
-            prev[0] = snake.rows[0];
-            prev[1] = snake.cols[0];
+    // Move the head of the snake
+    prev[0] = snake.rows[0];
+    prev[1] = snake.cols[0];
+    snake.rows[0] += snake.direction[0];
+    snake.cols[0] += snake.direction[1];
 
-            snake.rows[0] += snake.direction[0];
-            snake.cols[0] += snake.direction[1];
+    // Evaluate whether the head is in a valid position.
+    int c = mvinch(snake.rows[0], snake.cols[0]);
+    if (c == BORDER || c == SNAKE) {
+        game_should_continue = false;
+        return;
+    } else if (c == FOOD) {
+        // increment snake length
+        ate_food = true;
+    }
 
-            // Evaluate whether the head is in a valid position.
-            int c = mvinch(snake.rows[0], snake.cols[0]);
-            if (c == BORDER || c == SNAKE) {
-                game_should_continue = false;
-                return;
-            } else if (c == FOOD) {
-                // increment snake length
-                ate_food = true;
-            }
+    // Print the head.
+    mvaddch(snake.rows[0], snake.cols[0], SNAKE);
 
-            mvaddch(snake.rows[0], snake.cols[0], SNAKE);
+    // Update each segment with the position of its
+    // preceding segment.
+    for (int i = 1; i < snake.length; i++) {
+        int temp_row = snake.rows[i];
+        int temp_col = snake.cols[i];
 
-        } else {
-            int temp_row = snake.rows[i];
-            int temp_col = snake.cols[i];
+        snake.rows[i] = prev[0];
+        snake.cols[i] = prev[1];
 
-            snake.rows[i] = prev[0];
-            snake.cols[i] = prev[1];
-
-            prev[0] = temp_row;
-            prev[1] = temp_col;
-        }
+        prev[0] = temp_row;
+        prev[1] = temp_col;
     }
 
     if (ate_food) {
+        // Add a new segment to the snake.
+
         snake.length++;
 
         snake.rows = realloc(snake.rows, snake.length * sizeof *snake.rows);
@@ -189,6 +195,7 @@ void move_snake() {
         snake.rows[snake.length-1] = prev[0];
         snake.cols[snake.length-1] = prev[1];
 
+        // Place the food again.
         place_food();
     } else {
         mvaddch(prev[0], prev[1], EMPTY);
