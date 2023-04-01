0f379a9a147dc7d75a7fa745429f8f687705faab
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-04-01 15:30:28 +0100

    Add place_food logic

--- src/main.c
+++ src/main.c
@@ -39,6 +39,11 @@ struct snake {
     int direction[2];
 } snake;
 
+struct food {
+    int row;
+    int col;
+} food;
+
 /* -------------------------------------------------- */
 
 void init_screen() {
@@ -143,7 +148,44 @@ void move_snake() {
 
 /* -------------------------------------------------- */
 
-void init_food() {}
+void place_food() {
+    // Find all the spaces in which the food could be put.
+    int* empty_rows = malloc(0);
+    int* empty_cols = malloc(0);
+    int count = 0;
+    for (int i = 1; i < (screen.rows - 1); i++) {
+        for (int j = 1; j < (screen.cols - 1); j++) {
+            int pixel = mvinch(i,j);
+            if (pixel != EMPTY) {
+                continue;
+            }
+
+            count++;
+            empty_rows = realloc(empty_rows, count * sizeof *empty_rows);
+            empty_cols = realloc(empty_cols, count * sizeof *empty_cols);
+            empty_rows[count-1] = i;
+            empty_cols[count-1] = j;
+        }
+    }
+
+    int random_index = rand() % count;
+
+    if (food.row != NULL && food.col != NULL) {
+        mvaddch(food.row, food.col, EMPTY);
+    }
+    food.row = empty_rows[random_index];
+    food.col = empty_cols[random_index];
+    mvaddch(food.row, food.col, FOOD);
+    refresh();
+
+    free(empty_rows);
+    free(empty_cols);
+}
+
+void init_food() {
+    srand(time(NULL));
+    place_food();
+}
 
 void terminate_food() {}
 
