5f286de6cf3ac2f9813d107efe979e8ef8d0d30f
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-04-02 18:11:46 +0100

    Populate game .c and .h files

---
+++ src/game/src/game.c
@@ -0,0 +1,107 @@
+#include <ncurses.h>
+#include <time.h>
+#include <errno.h>
+#include <pthread.h>
+
+#include "food.h"
+#include "screen.h"
+#include "snake.h"
+#include "game.h"
+
+char* get_direction() {
+    if (snake.direction[0] == -1 && snake.direction[1] == 0) {
+        return "up";
+    } else if (snake.direction[0] == 1 && snake.direction[1] == 0) {
+        return "down";
+    } else if (snake.direction[0] == 0 && snake.direction[1] == -1) {
+        return "left";
+    } else /* if (snake.direction[0] == 0 && snake.direction[1] == 1) */ {
+        return "right";
+    }
+}
+
+bool is_direction(char* direction) {
+    return direction == get_direction();
+}
+
+void *getch_thread_fn(void *vargp) {
+    while (true) {
+        pressed_key = getch();
+        switch (pressed_key) {
+            case 'q':
+                game_should_continue = false;
+                break;
+
+            case 'w':
+            case KEY_UP:
+                if (!is_direction("down")) {
+                    snake.direction[0] = -1;
+                    snake.direction[1] = 0;
+                }
+                break;
+
+            case 'a':
+            case KEY_LEFT:
+                if (!is_direction("right")) {
+                    snake.direction[0] = 0;
+                    snake.direction[1] = -1;
+                }
+                break;
+
+            case 's':
+            case KEY_DOWN:
+                if (!is_direction("up")) {
+                    snake.direction[0] = 1;
+                    snake.direction[1] = 0;
+                }
+                break;
+
+            case 'd':
+            case KEY_RIGHT:
+                if (!is_direction("left")) {
+                    snake.direction[0] = 0;
+                    snake.direction[1] = 1;
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+}
+
+/* -------------------------------------------------- */
+
+// See https://stackoverflow.com/a/1157217
+int msleep(long msec) {
+    struct timespec ts;
+    int res;
+    if (msec < 0) { errno = EINVAL; return -1; }
+    ts.tv_sec = msec / 1000;
+    ts.tv_nsec = (msec % 1000) * 1000000;
+    do { res = nanosleep(&ts, &ts); } while (res && errno == EINTR);
+    return res;
+}
+
+/* -------------------------------------------------- */
+
+void init_game() {
+    init_screen();
+    init_snake();
+    init_food();
+    pthread_create(&ptid, NULL, &getch_thread_fn, NULL);
+}
+
+void run_game() {
+    while (game_should_continue) {
+        move_snake();
+        msleep(125);
+    }
+}
+
+void terminate_game() {
+    terminate_screen();
+    terminate_snake();
+    terminate_food();
+    pthread_cancel(ptid);
+}
---
+++ src/game/src/game.h
@@ -0,0 +1,34 @@
+#ifndef GAME_H_   /* Include guard */
+#define GAME_H_
+
+#include <stdbool.h>
+#include <pthread.h>
+
+#define SNAKE_INITIAL_LENGTH 1
+#define BORDER '#'
+#define SNAKE 'S'
+#define FOOD 'F'
+#define EMPTY ' '
+
+bool DEBUG = false;
+
+bool game_should_continue = true;
+
+int pressed_key;
+pthread_t ptid;
+
+char* get_direction();
+
+bool is_direction(char* direction);
+
+void *getch_thread_fn(void *vargp);
+
+int msleep(long msec);
+
+void init_game();
+
+void run_game();
+
+void terminate_game();
+
+#endif // GAME_H_
