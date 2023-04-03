24575b9199ff2cc7969ff090309f3059ad69264c
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-04-03 05:28:24 +0100

    Compile and link gui files with menu library

---
+++ src/gui/src/gui.c
@@ -0,0 +1,23 @@
+#include <menu.h>
+
+/*
+The general flow of control of a menu program looks like this.
+
+    1. Initialize curses
+
+    2. Create items using new_item(). You can specify a name and description for the items.
+
+    3. Create the menu with new_menu() by specifying the items to be attached with.
+
+    4. Post the menu with menu_post() and refresh the screen.
+
+    5. Process the user requests with a loop and do necessary updates to menu with menu_driver.
+
+    6. Unpost the menu with menu_unpost()
+
+    7. Free the memory allocated to menu by free_menu()
+
+    8. Free the memory allocated to the items with free_item()
+
+    9. End curses
+*/
